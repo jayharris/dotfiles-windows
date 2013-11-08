66da4c68c242437499c3b3565c0f8ee1f24a9aa4
Jay Harris <jay@aranasoft.com>
2013-11-08 12:40:02 -0500

    Added RSAT (with Workaround) to Chocolatey

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -13,4 +13,10 @@ cinst Fiddler
 cinst Firefox
 cinst Opera
 cinst SublimeText2
+# RSAT Package is not compatible w/ Win8.1 Enterprise, yet
+#cinst RSAT.FeaturePack
 cinst winmerge
+
+# Workaround for RSAT
+#Invoke-WebRequest "http://download.microsoft.com/download/1/8/E/18EA4843-C596-4542-9236-DE46F780806E/Windows8.1-KB2693643-x64.msu" -OutFile "$env:Temp\Windows8.1-KB2693643-x64.msu"
+#& wusa.exe "$env:Temp\Windows8.1-KB2693643-x64.msu" /quiet /norestart | Out-Null
