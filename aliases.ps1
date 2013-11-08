b9e559c379efd6ac71b436c5971be1ea23b60c99
Jay Harris <jay@aranasoft.com>
2013-11-08 00:39:41 -0500

    Workaround for PoSh not allowing function:.. assignment

--- aliases.ps1
+++ aliases.ps1
@@ -1,6 +1,7 @@
 # Easier Navigation: .., ..., ...., ....., and ~
 ${function:~} = { Set-Location ~ }
-${function:..} = { Set-Location .. }
+# PoSh won't allow ${function:..} because of an invalid path error, so...
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
 ${function:...} = { Set-Location ..\.. }
 ${function:....} = { Set-Location ..\..\.. }
 ${function:.....} = { Set-Location ..\..\..\.. }
