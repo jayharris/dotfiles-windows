b9e7bce96cf0ad849468d5aa531642a6dcf5306b
Jay Harris <jay@aranasoft.com>
2014-02-17 10:43:19 -0500

    Ensure that PoSh profile directory exists before copying files to it.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,5 @@
 $profileDir = Split-Path -parent $profile
+if (![System.IO.Directory]::Exists($profileDir)) {[System.IO.Directory]::CreateDirectory($profileDir)}
 Copy-Item -Path ./*.ps1 -Destination $profileDir -Exclude "bootstrap.ps1"
 Copy-Item -Path ./homeFiles/** -Destination $home -Include **
 Remove-Variable profileDir
