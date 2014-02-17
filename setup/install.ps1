d385a87ce4b377f9e7c40a59f82e824a5d097d66
Jay Harris <jay@aranasoft.com>
2014-02-17 10:35:52 -0500

    Beginnings of an install file

---
+++ setup/install.ps1
@@ -0,0 +1,54 @@
+$account = "jayharris"
+$repo    = "dotfiles-windows"
+$branch  = "master"
+
+$dotfilesTempDir = Join-Path $env:TEMP "dotfiles"
+if (![System.IO.Directory]::Exists($dotfilesTempDir)) {[System.IO.Directory]::CreateDirectory($dotfilesTempDir)}
+$sourceFile = Join-Path $dotfilesTempDir "dotfiles.zip"
+$dotfilesInstallDir = Join-Path $dotfilesTempDir "$repo-$branch"
+
+function Download-File {
+  param (
+    [string]$url,
+    [string]$file
+  )
+  Write-Host "Downloading $url to $file"
+  $downloader = new-object System.Net.WebClient
+  $downloader.DownloadFile($url, $file)
+}
+
+function Unzip-File {
+    param (
+        [string]$File,
+        [string]$Destination = (Get-Location).Path
+    )
+
+    $filePath = Resolve-Path $File
+    $destinationPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Destination)
+
+    If (($PSVersionTable.PSVersion.Major -ge 3) -and
+       ((Get-ItemProperty -Path "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full" -ErrorAction SilentlyContinue).Version -like "4.5*" -or
+       (Get-ItemProperty -Path "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Client" -ErrorAction SilentlyContinue).Version -like "4.5*")) {
+        try {
+            [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+            [System.IO.Compression.ZipFile]::ExtractToDirectory("$filePath", "$destinationPath")
+        } catch {
+            Write-Warning -Message "Unexpected Error. Error details: $_.Exception.Message"
+        }
+    } else {
+        try {
+            $shell = New-Object -ComObject Shell.Application
+            $shell.Namespace($destinationPath).copyhere(($shell.NameSpace($filePath)).items())
+        } catch {
+            Write-Warning -Message "Unexpected Error. Error details: $_.Exception.Message"
+        }
+    }
+}
+
+Download-File "https://github.com/$account/$repo/archive/$branch.zip" $sourceFile
+Unzip-File $sourceFile $dotfilesTempDir
+
+Push-Location $dotfilesInstallDir
+& .\bootstrap.ps1
+Pop-Location
+
