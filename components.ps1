2b2b1712edf1ffe63d3c0524029d1e2ee6f70773
Jay Harris <jay@aranasoft.com>
2014-02-28 08:20:26 -0500

    Added GitHub and VisualStudio to the executed components list

--- components.ps1
+++ components.ps1
@@ -2,5 +2,7 @@ Push-Location (Join-Path (Split-Path -parent $profile) "components")
 
 # From within the ./components directory...
 . .\coreaudio.ps1
+. .\githubforwindows.ps1
+. .\visualstudio.ps1
 
 Pop-Location
