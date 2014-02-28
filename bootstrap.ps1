7138a5b01dc1d10585173cdd01c2abea2e2dc3cd
Jay Harris <jay@aranasoft.com>
2014-02-28 07:46:46 -0500

    Fix issue with bootstrap not copying components

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,9 @@
 $profileDir = Split-Path -parent $profile
+$componentDir = Join-Path $profileDir "components"
 if (![System.IO.Directory]::Exists($profileDir)) {[System.IO.Directory]::CreateDirectory($profileDir)}
+if (![System.IO.Directory]::Exists($componentDir)) {[System.IO.Directory]::CreateDirectory($componentDir)}
 Copy-Item -Path ./*.ps1 -Destination $profileDir -Exclude "bootstrap.ps1"
+Copy-Item -Path ./components/** -Destination $componentDir -Include **
 Copy-Item -Path ./homeFiles/** -Destination $home -Include **
+Remove-Variable componentDir
 Remove-Variable profileDir
