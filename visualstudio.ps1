bc3ba1033c1ba8f0d58f4b419a0ff45715c9f4a6
Jay Harris <jay@aranasoft.com>
2014-02-28 09:19:43 -0500

    Update VSIX installer to latest version of Web Essentials 2013

--- visualstudio.ps1
+++ visualstudio.ps1
@@ -3,4 +3,4 @@ Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-
 # Productivity Power Tools for VS2013
 Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/3/ProPowerTools.vsix
 # Web Essentials 2013
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/31/WebEssentials2013.vsix
+Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/32/WebEssentials2013.vsix
