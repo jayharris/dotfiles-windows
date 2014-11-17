9d9ad768e54b6df75d856823c97724ecc2526b63
Jay Harris <jay@aranasoft.com>
2014-11-17 14:37:46 -0500

    Update VSIX refs to latest VsVim and Power Tools

--- visualstudio.ps1
+++ visualstudio.ps1
@@ -1,6 +1,6 @@
 # VsVim
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/46/VsVim.vsix
+Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/54/VsVim.vsix
 # Productivity Power Tools for VS2013
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/3/ProPowerTools.vsix
+Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
 # Web Essentials 2013
 Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/32/WebEssentials2013.vsix
