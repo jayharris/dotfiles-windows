72501966390250368be65b9eb6bc72fe1ebda526
Jay Harris <jay@aranasoft.com>
2015-03-20 15:33:47 -0400

    Fix load order of Exports

--- profile.ps1
+++ profile.ps1
@@ -2,5 +2,5 @@
 # ===========
 
 Push-Location (Split-Path -parent $profile)
-"exports","components","functions","aliases","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+"components","functions","aliases","exports","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
