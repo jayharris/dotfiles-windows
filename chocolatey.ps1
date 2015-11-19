43137d54a74e740c3925f3f83509562bf025c650
Jay Harris <jay@aranasoft.com>
2015-11-18 21:20:13 -0500

    feat(confirm): Enable `allowGlobalConfirmation`, Issue #6

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,6 +1,7 @@
 if ((which cinst) -eq $null) {
     iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    choco feature enable -n=allowGlobalConfirmation
 }
 
 #cinst curl #`curl` comes with GH4W
