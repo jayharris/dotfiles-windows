71db0cd6fa5c7c682d37ac03e5731455d9748c33
Jay Harris <jay@aranasoft.com>
2016-02-24 17:22:38 -0500

    chore(deps): update vs2013 extensions to latest

--- visualstudio.ps1
+++ visualstudio.ps1
@@ -1,6 +1,7 @@
+### Visual Studio 2013
 # VsVim
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/54/VsVim.vsix
-# Productivity Power Tools for VS2013
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
+Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
+# Productivity Power Tools 2013
+Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
 # Web Essentials 2013
-Install-VSExtension http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/32/WebEssentials2013.vsix
+Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
