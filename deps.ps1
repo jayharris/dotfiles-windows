9c496e17f4e255f5b2cfff097f26f7db2c3141ae
Jay Harris <jay@aranasoft.com>
2016-02-26 12:32:38 -0500

    feat(win10): update windows and dependency settings for Windows 10
     - Move Windows Feature installation to `deps.ps1`
     - Improve handling GH4W installation completion
     - Update Windows Update policies for Windows 10
     - Disable Cortana
     - Update Bing Search settings for Windows 10
     - Reorganize structure of `windows.ps1`

--- deps.ps1
+++ deps.ps1
@@ -9,6 +9,7 @@ Set-PackageSource -Name chocolatey -Trusted
 ### Chocolatey Packages
 Install-Package -Provider Chocolatey -Name curl #`curl` comes with GH4W
 Install-Package -Provider Chocolatey -Name Dropbox
+Install-Package -Provider Chocolatey -Name GitHubForWindows
 Install-Package -Provider Chocolatey -Name GoogleChrome
 Install-Package -Provider Chocolatey -Name GoogleChrome.Canary
 Install-Package -Provider Chocolatey -Name hg
@@ -23,25 +24,26 @@ Install-Package -Provider Chocolatey -Name wput
 Install-Package -Provider Chocolatey -Name winmerge
 
 
-### Git with SSH via GitHubForWindows
-Install-Package -Provider Chocolatey -Name GitHubForWindows | Out-Null
-
-Write-Host ""
-Write-Host "GitHubForWindows installation requires user interaction."
-Write-Host "Setup is paused while you complete the installation."
-Write-Host ""
-Read-Host -Prompt "When installation has completed, press Enter to continue" | Out-Null
-Push-Location (Join-Path $env:LocalAppData "GitHub")
-Write-Host ""
-Write-Host "Relaunching GitHubForWindows to begin tooling installation."
-Write-Host "Once launched, close GitHubForWindows to proceed."
-Start-Process .\GitHub.appref-ms -Wait
-Write-Host ""
-Write-Host "Launching GitHubForWindows Shell to complete tooling installation."
-Start-Process .\GitHub.appref-ms -ArgumentList "--open-shell"
-Read-Host -Prompt "After launching, close the GH4W shell and press Enter to proceed" | Out-Null
-. (Join-Path (Split-Path -parent $PROFILE) "profile.ps1")
-Pop-Location
+### Completing PoshGit installation if installing GH4W
+if (((Get-Package -Name "GitHubForWindows" -ErrorAction SilentlyContinue) -ne $null) -and -not (Get-Command git -ErrorAction SilentlyContinue | Test-Path)) {
+    Write-Host ""
+    Write-Host "You have installed GitHubForWindows but `git` was not found."
+    Write-Host "In case GitHubForWindows is newly installed, execution has been"
+    Write-Host "paused while you complete the installation."
+    Write-Host ""
+    Read-Host -Prompt "When installation has completed, press Enter to continue" | Out-Null
+    Push-Location (Join-Path $env:LocalAppData "GitHub")
+        Write-Host ""
+        Write-Host "Relaunching GitHubForWindows to begin tooling installation."
+        Write-Host "Once launched, close GitHubForWindows to proceed."
+        Start-Process .\GitHub.appref-ms -Wait
+        Write-Host ""
+        Write-Host "Launching GitHubForWindows Shell to complete tooling installation."
+        Start-Process .\GitHub.appref-ms -ArgumentList "--open-shell"
+        Read-Host -Prompt "After launching, close the GH4W shell and press Enter to proceed" | Out-Null
+        . (Join-Path (Split-Path -parent $PROFILE) "profile.ps1")
+    Pop-Location
+}
 
 
 ### Node Packages
@@ -61,6 +63,41 @@ npm install -g yo
 
 ### Install Janus for vim
 curl.exe -L https://bit.ly/janus-bootstrap | bash
+
+
+### Windows Features
+& dism.exe /Online /Enable-Feature /All `
+    /FeatureName:NetFx3 `
+    /FeatureName:IIS-WebServerRole `
+    /FeatureName:IIS-WebServer `
+    /FeatureName:IIS-CommonHttpFeatures `
+    /FeatureName:IIS-HttpErrors `
+    /FeatureName:IIS-ApplicationDevelopment `
+    /FeatureName:IIS-NetFxExtensibility `
+    /FeatureName:IIS-NetFxExtensibility45 `
+    /FeatureName:IIS-HealthAndDiagnostics `
+    /FeatureName:IIS-HttpLogging `
+    /FeatureName:IIS-Security `
+    /FeatureName:IIS-RequestFiltering `
+    /FeatureName:IIS-Performance `
+    /FeatureName:IIS-HttpCompressionDynamic `
+    /FeatureName:IIS-WebServerManagementTools `
+    /FeatureName:IIS-WindowsAuthentication `
+    /FeatureName:IIS-StaticContent `
+    /FeatureName:IIS-DefaultDocument `
+    /FeatureName:IIS-DirectoryBrowsing `
+    /FeatureName:IIS-WebSockets `
+    /FeatureName:IIS-ASPNET `
+    /FeatureName:IIS-ASPNET45 `
+    /FeatureName:IIS-ISAPIExtensions `
+    /FeatureName:IIS-ISAPIFilter `
+    /FeatureName:IIS-BasicAuthentication `
+    /FeatureName:IIS-HttpCompressionStatic `
+    /FeatureName:IIS-ManagementConsole `
+    /FeatureName:WCF-Services45 `
+    /FeatureName:WCF-TCP-PortSharing45 `
+    /FeatureName:NetFx4-AdvSrvs `
+    /FeatureName:NetFx4Extended-ASPNET45 | Out-Null
 
 
 ### Visual Studio 2015
--- windows.ps1
+++ windows.ps1
@@ -31,43 +31,12 @@ Remove-Variable myPrincipal
 Remove-Variable myIdentity
 
 
-# Configure IIS
-Write-Output "Configuring IIS. This may take a while..."
-& dism.exe /Online /Enable-Feature /All `
-    /FeatureName:NetFx3 `
-    /FeatureName:IIS-WebServerRole `
-    /FeatureName:IIS-WebServer `
-    /FeatureName:IIS-CommonHttpFeatures `
-    /FeatureName:IIS-HttpErrors `
-    /FeatureName:IIS-ApplicationDevelopment `
-    /FeatureName:IIS-NetFxExtensibility `
-    /FeatureName:IIS-NetFxExtensibility45 `
-    /FeatureName:IIS-HealthAndDiagnostics `
-    /FeatureName:IIS-HttpLogging `
-    /FeatureName:IIS-Security `
-    /FeatureName:IIS-RequestFiltering `
-    /FeatureName:IIS-Performance `
-    /FeatureName:IIS-HttpCompressionDynamic `
-    /FeatureName:IIS-WebServerManagementTools `
-    /FeatureName:IIS-WindowsAuthentication `
-    /FeatureName:IIS-StaticContent `
-    /FeatureName:IIS-DefaultDocument `
-    /FeatureName:IIS-DirectoryBrowsing `
-    /FeatureName:IIS-WebSockets `
-    /FeatureName:IIS-ASPNET `
-    /FeatureName:IIS-ASPNET45 `
-    /FeatureName:IIS-ISAPIExtensions `
-    /FeatureName:IIS-ISAPIFilter `
-    /FeatureName:IIS-BasicAuthentication `
-    /FeatureName:IIS-HttpCompressionStatic `
-    /FeatureName:IIS-ManagementConsole `
-    /FeatureName:WCF-Services45 `
-    /FeatureName:WCF-TCP-PortSharing45 `
-    /FeatureName:NetFx4-AdvSrvs `
-    /FeatureName:NetFx4Extended-ASPNET45 | Out-Null
-
 # HKUsers drive for Registry
 if ((Get-PSDrive HKUsers -ErrorAction SilentlyContinue) -eq $null) { New-PSDrive -Name HKUSERS -PSProvider Registry -Root Registry::HKEY_USERS | Out-Null }
+
+
+### Devices, Power, and Startup
+### --------------------------
 
 # Sound: Disable Startup Sound
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" "DisableStartupSound" 1
@@ -79,10 +48,14 @@ powercfg /hibernate off
 # Power Set standby delay to 24 hours
 powercfg /change /standby-timeout-ac 1440
 
+# SSD: Disable SuperFetch
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" "EnableSuperfetch" 0
+
 
 ### Explorer, Taskbar, and System Tray
 ### --------------------------
-if (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {New-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Type Folder | Out-Null}
+if (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Type Folder | Out-Null}
+if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search")) {New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" -Type Folder | Out-Null}
 
 # Explorer: Show hidden files by default (1: Show Files, 2: Hide Files)
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1
@@ -102,28 +75,21 @@ Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advan
 # Taskbar: Don't show Windows Store Apps on Taskbar
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "StoreAppsOnTaskbar" 0
 
+# Taskbar: Disable Bing Search
+# Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\ConnectedSearch" "ConnectedSearchUseWeb" 0 # For Windows 8.1
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" "BingSearchEnabled" 0 # For Windows 10
+
+# Taskbar: Disable Cortana
+Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" "AllowCortana" 0
+
 # SysTray: hide the Action Center, Network, and Volume icons
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAHealth" 1
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCANetwork" 1
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAVolume" 1
-#Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAPower" 1
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAHealth" 1  # Action Center
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCANetwork" 1 # Network
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAVolume" 1  # Volume
+#Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "HideSCAPower" 1  # Power
 
 # Recycle Bin: Disable Delete Confirmation Dialog
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "ConfirmFileDelete" 0
-
-
-### Charm Bar
-### --------------------------
-
-# Disable Bing Search
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\ConnectedSearch" "ConnectedSearchUseWeb" 0
-
-
-### SSD Specific Tweaks
-### --------------------------
-
-# Disable SuperFetch
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" "EnableSuperfetch" 0
 
 
 ### Lock Screen
@@ -133,6 +99,7 @@ Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory 
 ## Background file: C:\someDirectory\someImage.jpg
 ## File Size Limit: 256Kb
 # Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Personalization" "LockScreenImage" "C:\someDirectory\someImage.jpg"
+
 
 ### Accessibility
 ### --------------------------
@@ -145,20 +112,24 @@ Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File 
 ### Windows Update
 ### --------------------------
 
-$AUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
-# Windows Update: Auto-Download but not Install. 0=NotConfigured, 1=Disabled, 2=NotifyBeforeDownload, 3=NotifyBeforeInstall, 4=ScheduledInstall
-$AUSettings.NotificationLevel = 3
-# Windows Update: Include Recommended Updates
-$AUSettings.IncludeRecommendedUpdates = $true
-$AUSettings.Save | Out-Null
-Remove-Variable AUSettings
+if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate")) {New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -Type Folder | Out-Null}
+if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU")) {New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Type Folder | Out-Null}
 
-if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate")) {New-Item -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Type Folder | Out-Null}
+# Windows Update: Enable Automatic Updates. 0=Enabled, 1=Disabled
+Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" "NoAutoUpdate" 0
+
 # Windows Update: Don't automatically reboot after install
 Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" "NoAutoRebootWithLoggedOnUsers" 1d
+Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" "NoAutoRebootWithLoggedOnUsers" 1
+
+# Windows Update: Auto-Download but not Install. 0=NotConfigured, 1=Disabled, 2=NotifyBeforeDownload, 3=NotifyBeforeInstall, 4=ScheduledInstall
+Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" "AUOptions" 3
+
+# Windows Update: Include Recommended Updates
+Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" "IncludeRecommendedUpdates" 1
 
 # Windows Update: Opt-In to Microsoft Update
-$MU = New-Object -ComObject Microsoft.Update.ServiceManager -Strict 
+$MU = New-Object -ComObject Microsoft.Update.ServiceManager -Strict
 $MU.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
 Remove-Variable MU
 
