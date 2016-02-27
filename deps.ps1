c2473d4a050480cd7d0e8f74d66b1295b4e52e38
Jay Harris <jay@aranasoft.com>
2016-02-27 16:46:55 -0500

    feat(deps): wrap dependency installations within tool checks

--- deps.ps1
+++ deps.ps1
@@ -59,45 +59,53 @@ if (((choco list -lr | where {$_ -like "githubforwindows*"}) -ne $null) -and ((w
 
 
 ### Web Platform Installer
-webpicmd /Install /AcceptEula /Products:"StaticContent,DefaultDocument,DirectoryBrowse,RequestFiltering,HTTPErrors,HTTPLogging,ISAPIExtensions,ISAPIFilters,UrlRewrite2"
-webpicmd /Install /AcceptEula /Products:"BasicAuthentication,WindowsAuthentication"
-webpicmd /Install /AcceptEula /Products:"StaticContentCompression,DynamicContentCompression"
-webpicmd /Install /AcceptEula /Products:"IISManagementConsole"
-webpicmd /Install /AcceptEula /Products:"WebSockets"
-webpicmd /Install /AcceptEula /Products:"NetFx3,NetFx4,NETFramework452,NetFx4Extended-ASPNET45,NETExtensibility,NetFxExtensibility45,ASPNET,ASPNET45"
-webpicmd /Install /AcceptEula /Products:"Python279"
+if (which webpicmd) {
+    webpicmd /Install /AcceptEula /Products:"StaticContent,DefaultDocument,DirectoryBrowse,RequestFiltering,HTTPErrors,HTTPLogging,ISAPIExtensions,ISAPIFilters,UrlRewrite2"
+    webpicmd /Install /AcceptEula /Products:"BasicAuthentication,WindowsAuthentication"
+    webpicmd /Install /AcceptEula /Products:"StaticContentCompression,DynamicContentCompression"
+    webpicmd /Install /AcceptEula /Products:"IISManagementConsole"
+    webpicmd /Install /AcceptEula /Products:"WebSockets"
+    webpicmd /Install /AcceptEula /Products:"NetFx3,NetFx4,NETFramework452,NetFx4Extended-ASPNET45,NETExtensibility,NetFxExtensibility45,ASPNET,ASPNET45"
+    webpicmd /Install /AcceptEula /Products:"Python279"
+}
 
 
 ### Node Packages
-npm install -g azure-cli
-npm install -g bower
-npm install -g coffee-script
-npm install -g conventional-changelog
-npm install -g grunt
-npm install -g gulp
-npm install -g less
-npm install -g lineman
-npm install -g mocha
-npm install -g node-inspector
-npm install -g sass
-npm install -g yo
+if (which npm) {
+    npm install -g azure-cli
+    npm install -g bower
+    npm install -g coffee-script
+    npm install -g conventional-changelog
+    npm install -g grunt
+    npm install -g gulp
+    npm install -g less
+    npm install -g lineman
+    npm install -g mocha
+    npm install -g node-inspector
+    npm install -g sass
+    npm install -g yo
+}
 
 
-### Install Janus for vim
-curl.exe -L https://bit.ly/janus-bootstrap | bash
+### Janus for vim
+if ((which vim) -and (which rake)) {
+    curl.exe -L https://bit.ly/janus-bootstrap | bash
+}
 
 
-### Visual Studio 2015
-# VsVim
-Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-# Productivity Power Tools 2015
-Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
+### Visual Studio Plugins
+if (which Install-VSExtension) {
+    ### Visual Studio 2015
+    # VsVim
+    Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
+    # Productivity Power Tools 2015
+    Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
 
-
-### Visual Studio 2013
-# VsVim
-# Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-# Productivity Power Tools 2013
-# Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
-# Web Essentials 2013
-# Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
+    ### Visual Studio 2013
+    # VsVim
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
+    # Productivity Power Tools 2013
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
+    # Web Essentials 2013
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
+}
