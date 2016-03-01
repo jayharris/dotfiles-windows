jayharris/dotfiles-windows

commit 348548f86b929dff8b9f800b93a026aedb171865

fix(var): clean up leftover variables from profile setup

windows.ps1
@@ -226,14 +226,12 @@ Set-ItemProperty "HKCU:\Console\PSReadLine" "EmphasisForeground" 0xD
 # PSReadLine: Error syntax color. vim Error group. (Default: 0xC)
 Set-ItemProperty "HKCU:\Console\PSReadLine" "ErrorForeground" 0x4
 
-$registryPaths=@(`
+@(`
 "HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe",`
 "HKCU:\Console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe",`
 "HKCU:\Console\Windows PowerShell (x86)",`
 "HKCU:\Console\Windows PowerShell"`
-)
-
-$registryPaths | ForEach {
+) | ForEach {
     If (!(Test-Path $_)) {
         New-Item -path $_ -ItemType Folder | Out-Null
     }
@@ -241,7 +239,9 @@ $registryPaths | ForEach {
     ForEach ($setting in $settings.GetEnumerator()) {
         Set-ItemProperty -Path $_ -Name $($setting.Name) -Value $($setting.Value)
     }
+    Remove-Variable setting
 }
+Remove-Setting settings
 
 Reset-AllPowerShellShortcuts
 

components/visualstudio.ps1
@@ -8,27 +8,32 @@ if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7") -or (Test-Path "
     if ($vsRegistry -eq $null) { $vsRegistry = Get-Item "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7" }
     $vsVersion  = $vsRegistry.property | Sort-Object -Descending | Select-Object -first 1
     $vsinstall  = ForEach-Object -process {$vsRegistry.GetValue($vsVersion)}
+    Remove-Variable vsRegistry
 
     if ((Test-Path $vsinstall) -eq 0) {Write-Error "Unable to find Visual Studio installation"}
 
     $env:VisualStudioVersion = $vsVersion
     $env:Framework40Version  = "v4.0"
     $env:VSINSTALLDIR = $vsinstall
-    $env:DevEnvDir    =  Join-Path ${vsinstall} "Common7\IDE\"
+    $env:DevEnvDir    =  Join-Path $vsinstall "Common7\IDE\"
     if (!($env:Path).Split(";").Contains($vsinstall)) { Append-EnvPath $vsinstall }
     $vsCommonToolsVersion = $vsVersion.replace(".","")
     Invoke-Expression "`$env:VS${vsCommonToolsVersion}COMN = Join-Path `"$vsinstall`" `"Common7\Tools`""
     Invoke-Expression "`$env:VS${vsCommonToolsVersion}COMNTOOLS = Join-Path `"$vsinstall`" `"Common7\Tools`""
+    Remove-Variable vsCommonToolsVersion
+    Remove-Variable vsinstall
 
     $vsSetupRegistryPath = "hklm:\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\$vsVersion\Setup"
     if (${Test-Path $vsSetupRegistryPath} -eq $false) { $vsSetupRegistryPath = "hklm:\SOFTWARE\Microsoft\VisualStudio\$vsVersion\Setup" }
     $vsVersionUser = (Get-ChildItem $vsSetupRegistryPath -Name | Where-Object {$_ -like "Visual Studio * Prereq*"} | Select-Object -First 1).Substring(14,4)
     $env:GYP_MSVS_VERSION = $vsVersionUser
+    Remove-Variable vsVersionUser
+    Remove-Variable vsSetupRegistryPath
 
-    Append-EnvPathIfExists (Join-Path $vsinstall "Common7\Tools")
-    Append-EnvPathIfExists (Join-Path $vsinstall "Team Tools\Performance Tools")
-    Append-EnvPathIfExists (Join-Path $vsinstall "VSTSDB\Deploy")
-    Append-EnvPathIfExists (Join-Path $vsinstall "CommonExtensions\Microsoft\TestWindow")
+    Append-EnvPathIfExists (Join-Path $env:VSINSTALLDIR "Common7\Tools")
+    Append-EnvPathIfExists (Join-Path $env:VSINSTALLDIR "Team Tools\Performance Tools")
+    Append-EnvPathIfExists (Join-Path $env:VSINSTALLDIR "VSTSDB\Deploy")
+    Append-EnvPathIfExists (Join-Path $env:VSINSTALLDIR "CommonExtensions\Microsoft\TestWindow")
     Append-EnvPathIfExists (Join-Path $env:ProgramFiles "MSBuild\$vsVersion\bin")
     Append-EnvPathIfExists (Join-Path ${env:ProgramFiles(x86)} "MSBuild\$vsVersion\bin")
     Append-EnvPathIfExists (Join-Path $env:ProgramFiles "Microsoft SDKs\TypeScript")
@@ -36,17 +41,19 @@ if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7") -or (Test-Path "
     Append-EnvPathIfExists (Join-Path $env:ProgramFiles "HTML Help Workshop")
     Append-EnvPathIfExists (Join-Path ${env:ProgramFiles(x86)} "HTML Help Workshop")
 
+    Remove-Variable vsVersion
 
     if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VC7") -or (Test-Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VC7")) {
         $vcRegistry = Get-Item "hklm:\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VC7" -ErrorAction SilentlyContinue
         if ($vcRegistry -eq $null) { $vcRegistry = Get-Item "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VC7" }
-        $env:VCINSTALLDIR   = $vcRegistry.GetValue($vsVersion)
+        $env:VCINSTALLDIR   = $vcRegistry.GetValue($env:VisualStudioVersion)
         $env:FrameworkDir32 = $vcRegistry.GetValue("FrameworkDir32")
         $env:FrameworkDir64 = $vcRegistry.GetValue("FrameworkDir64")
         $env:FrameworkDir   = $env:FrameworkDir32
         $env:FrameworkVersion32 = $vcRegistry.GetValue("FrameworkVer32")
         $env:FrameworkVersion64 = $vcRegistry.GetValue("FrameworkVer64")
         $env:FrameworkVersion   = $env:FrameworkVersion32
+        Remove-Variable vcRegistry
 
         Append-EnvPathIfExists (Join-Path $env:FrameworkDir $env:Framework40Version)
         Append-EnvPathIfExists (Join-Path $env:FrameworkDir $env:FrameworkVersion)
@@ -81,7 +88,14 @@ if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7") -or (Test-Path "
             $vcTargetsPathValue = $msBuildRegistry.GetValue($_)
             $vcTargetsPath = $vcTargetsPathValue.Substring($vcTargetsPathValue.IndexOf("`$(MSBuildExtensionsPath32)")+26).TrimEnd("')")
             Invoke-Expression "`$env:${_} = Join-Path `"$msBuildToolsRoot`" `"$vcTargetsPath`""
+            Remove-Variable vcTargetsPath
+            Remove-Variable vcTargetsPathValue
         }
+
+        Remove-Variable msBuildVersion
+        Remove-Variable msBuildToolsRoot
+        Remove-Variable msBuildRegistry
+        Remove-Variable msBuildToolsRegistryPath
     }
 
     if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\$vsVersion\Setup\F#") -or (Test-Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\$vsVersion\Setup\F#")) {
@@ -89,31 +103,33 @@ if ((Test-Path "hklm:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7") -or (Test-Path "
         if ($fsRegistry -eq $null) { $fsRegistry = Get-Item "hklm:\SOFTWARE\Microsoft\VisualStudio\$vsVersion\Setup\F#" }
         $env:FSHARPINSTALLDIR = $fsRegistry.GetValue("ProductDir")
         Append-EnvPathIfExists $env:FSHARPINSTALLDIR
+        Remove-Variable fsRegistry
     }
+
 
     # Configure Visual Studio functions
     function Start-VisualStudio ([string] $solutionFile) {
-        $devenv = Resolve-Path "$vsinstall\Common7\IDE\devenv.exe"
+        $devenv = Join-Path $env:DevEnvDir "devenv.exe"
         if (($solutionFile -eq $null) -or ($solutionFile -eq "")) {
             $solutionFile = (Get-ChildItem -Filter "*.sln" | Select-Object -First 1).Name
         }
         if (($solutionFile -ne $null) -and ($solutionFile -ne "") -and (Test-Path $solutionFile)) {
-            start-process $devenv -ArgumentList $solutionFile
+            Start-Process $devenv -ArgumentList $solutionFile
         } else {
-            start-process $devenv
+            Start-Process $devenv
         }
     }
     Set-Alias -name vs -Value Start-VisualStudio
 
     function Start-VisualStudioAsAdmin ([string] $solutionFile) {
-        $devenv = Resolve-Path "$vsinstall\Common7\IDE\devenv.exe"
+        $devenv = Join-Path $env:DevEnvDir "devenv.exe"
         if (($solutionFile -eq $null) -or ($solutionFile -eq "")) {
             $solutionFile = (Get-ChildItem -Filter "*.sln" | Select-Object -First 1).Name
         }
         if (($solutionFile -ne $null) -and ($solutionFile -ne "") -and (Test-Path $solutionFile)) {
-            start-process $devenv -ArgumentList $solutionFile -verb "runAs"
+            Start-Process $devenv -ArgumentList $solutionFile -Verb "runAs"
         } else {
-            start-process $devenv -verb "runAs"
+            Start-Process $devenv -Verb "runAs"
         }
     }
     Set-Alias -name vsadmin -Value Start-VisualStudioAsAdmin

commit c1f9634215b8113f50244f9a24081307b1d169c9

fix(parse): correct parsing errors introduced in 348548f and 0555f1c

windows.ps1
@@ -150,35 +150,34 @@ Set-ItemProperty "HKCU:\Software\Microsoft\Internet Explorer\Main" "Start Page" 
 $diskCleanupRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\"
 
 # Cleanup Files by Group. 0=Disabled, 2=Enabled
-Set-ItemProperty Join-Path($diskCleanupRegPath "Active Setup Temp Folders"                    ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "BranchCache"                                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Downloaded Program Files"                     ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Internet Cache Files"                         ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Offline Pages Files"                          ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Old ChkDsk Files"                             ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Previous Installations"                       ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Recycle Bin"                                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "RetailDemo Offline Content"                   ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Service Pack Cleanup"                         ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Setup Log Files"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "System error memory dump files"               ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "System error minidump files"                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Temporary Files"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Temporary Setup Files"                        ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Thumbnail Cache"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Update Cleanup"                               ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Upgrade Discarded Files"                      ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "User file versions"                           ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Defender"                             ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Error Reporting Archive Files"        ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Error Reporting Queue Files"          ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Error Reporting System Archive Files" ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Error Reporting System Queue Files"   ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Error Reporting Temp Files"           ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows ESD installation files"               ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
-Set-ItemProperty Join-Path($diskCleanupRegPath "Windows Upgrade Log Files"                    ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "BranchCache"                                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Downloaded Program Files"                     ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Internet Cache Files"                         ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Offline Pages Files"                          ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Old ChkDsk Files"                             ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Previous Installations"                       ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Recycle Bin"                                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "RetailDemo Offline Content"                   ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Service Pack Cleanup"                         ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Setup Log Files"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "System error memory dump files"               ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "System error minidump files"                  ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Temporary Files"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Temporary Setup Files"                        ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Thumbnail Cache"                              ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Update Cleanup"                               ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Upgrade Discarded Files"                      ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "User file versions"                           ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Defender"                             ) "StateFlags6174" 2   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Error Reporting Archive Files"        ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Error Reporting Queue Files"          ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Error Reporting System Archive Files" ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Error Reporting System Queue Files"   ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Error Reporting Temp Files"           ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows ESD installation files"               ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
+Set-ItemProperty $(Join-Path $diskCleanupRegPath "Windows Upgrade Log Files"                    ) "StateFlags6174" 0   -ErrorAction SilentlyContinue
 
-Remove-Variable $diskCleanupRegPath
+Remove-Variable diskCleanupRegPath
 
 
 ### PowerShell Console
@@ -277,7 +276,7 @@ Set-ItemProperty "HKCU:\Console\PSReadLine" "ErrorForeground" 0x4
     }
     Remove-Variable setting
 }
-Remove-Setting settings
+Remove-Variable settings
 
 Reset-AllPowerShellShortcuts
 