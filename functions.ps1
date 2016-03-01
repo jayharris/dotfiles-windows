0d65d943c77a68178361ce37bc6ac9ee0d0efbbf
Jay Harris <jay@aranasoft.com>
2016-03-01 00:02:29 -0500

    fix(env): manually persist Environment registry keys
    Fixes #11

--- functions.ps1
+++ functions.ps1
@@ -92,7 +92,9 @@ function Refresh-Environment {
 
 # Set a permanent Environment variable, and reload it into $env
 function Set-Environment([String] $variable, [String] $value) {
-    [System.Environment]::SetEnvironmentVariable("$variable", "$value","User")
+    Set-ItemProperty "HKCU:\Environment" $variable $value
+    # Manually setting Registry entry. SetEnvironmentVariable is too slow because of blocking HWND_BROADCAST
+    #[System.Environment]::SetEnvironmentVariable("$variable", "$value","User")
     Invoke-Expression "`$env:${variable} = `"$value`""
 }
 
