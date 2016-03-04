e6b2b2dce44999c3f5f2a530056cf987a63b4d33
Jay Harris <jay@aranasoft.com>
2016-03-03 20:56:03 -0500

    feat(aero): disable Window Snap automatic window arrangement
    Fixes #13

--- windows.ps1
+++ windows.ps1
@@ -187,12 +187,24 @@ Get-AppxPackage "Microsoft.ZuneVideo" | Remove-AppxPackage
 # Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Personalization" "LockScreenImage" "C:\someDirectory\someImage.jpg"
 
 
-### Accessibility
+### Accessibility and Ease of Use
 ### --------------------------
 
 # Turn Off Windows Narrator
 if (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Narrator.exe")) {New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Narrator.exe" -Type Folder | Out-Null}
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Narrator.exe" "Debugger" "%1"
+
+# Disable "Window Snap" Automatic Window Arrangement
+Set-ItemProperty "HKCU:\Control Panel\Desktop" "WindowArrangementActive" 0
+
+# Disable automatic fill to space on Window Snap
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "SnapFill" 0
+
+# Disable showing what can be snapped next to a window
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "SnapAssist" 0
+
+# Disable automatic resize of adjacent windows on snap
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "JointResize" 0
 
 
 ### Windows Update
