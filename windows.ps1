f646fcc4c1782b2319f8e9a3b61bc350e4d550c2
Jay Harris <jay@aranasoft.com>
2017-03-25 23:46:21 -0400

    feat(privacy): add speech, ink, and text privacy settings

--- windows.ps1
+++ windows.ps1
@@ -32,63 +32,69 @@ if (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo
 if (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Input")) {New-Item -Path "HKCU:\SOFTWARE\Microsoft\Input" -Type Folder | Out-Null}
 if (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC")) {New-Item -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC" -Type Folder | Out-Null}
 
-# General: Let apps use my advertising ID for experiences across apps: Allow: 1, Disallow: 0
+# General: Don't let apps use advertising ID for experiences across apps: Allow: 1, Disallow: 0
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" "Enabled" 0
 
-# General: Turn on SmartScreen Filter to check web content (URLs) that Windows Store apps use: Allow: 1, Disallow: 0
+# General: Disable SmartScreen Filter: Enable: 1, Disable: 0
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" "EnableWebContentEvaluation" 0
 
-# General: Keylogging: Send Microsoft info about how I write...: Allow: 1, Disallow: 0
+# General: Disable key logging & transmission to Microsoft: Enable: 1, Disable: 0
 # Disabled when Telemetry is set to Basic
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Input\TIPC" "Enabled" 0
 
-# General: Let websites provide locally relevant content by accessing my language list: Allow: 0, Disallow 1
+# General: Opt-out from websites from accessing language list: Opt-in: 0, Opt-out 1
 Set-ItemProperty "HKCU:\Control Panel\International\User Profile" "HttpAcceptLanguageOptOut" 1
 
-# General: SmartGlass: Let apps on other devices open apps on this device: Allow: 1, Disallow: 0
+# General: Disable SmartGlass: Enable: 1, Disable: 0
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\SmartGlass" "UserAuthPolicy" 0
 
-# General: SmartGlass: Let apps on other devices use Bluetooth to open apps on this device: Allow: 1, Disallow: 0
+# General: Disable SmartGlass over BlueTooth: Enable: 1, Disable: 0
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\SmartGlass" "BluetoothPolicy" 0
 
-# Camera: Let apps use camera: Allow, Deny
+# Camera: Don't let apps use camera: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{E5323777-F976-4f5b-9B55-B94699C46E44}" "Value" "Deny"
 
-# Microphone: Let apps use microphone: Allow, Deny
+# Microphone: Don't let apps use microphone: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{2EEF81BE-33FA-4800-9670-1CD474972C3F}" "Value" "Deny"
 
-# Notifications: Let apps access notifications: Allow, Deny
+# Notifications: Don't let apps access notifications: Allow, Deny
 # Build 1511
 # Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{21157C1F-2651-4CC1-90CA-1F28B02263F6}" "Value" "Deny"
 # Build 1607
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{52079E78-A92B-413F-B213-E8FE35712E72}" "Value" "Deny"
 
-# Account Info: Let apps access name, picture, and other account info: Allow, Deny
+# Speech, Inking, & Typing: Stop "Getting to know me"
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" "AcceptedPrivacyPolicy" 0
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" "HarvestContacts" 0
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization" "RestrictImplicitTextCollection" 1
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization" "RestrictImplicitInkCollection" 1
+
+# Account Info: Don't let apps access name, picture, and other account info: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{C1D23ACC-752B-43E5-8448-8D0E519CD6D6}" "Value" "Deny"
 
-# Contacts: Let apps access contacts: Allow, Deny
+# Contacts: Don't let apps access contacts: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{7D7E8402-7C54-4821-A34E-AEEFD62DED93}" "Value" "Deny"
 
-# Calendar: Let apps access calendar: Allow, Deny
+# Calendar: Don't let apps access calendar: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{D89823BA-7180-4B81-B50C-7E471E6121A3}" "Value" "Deny"
 
-# Call History: Let apps access call history: Allow, Deny
+# Call History: Don't let apps access call history: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{8BC668CF-7728-45BD-93F8-CF2B3B41D7AB}" "Value" "Deny"
 
-# Email: Let apps read and send email: Allow, Deny
+# Email: Don't let apps read and send email: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{9231CB4C-BF57-4AF3-8C55-FDA7BFCC04C5}" "Value" "Deny"
 
-# Messaging: Let apps read or send messages (text or MMS): Allow, Deny
+# Messaging: Don't let apps read or send messages (text or MMS): Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{992AFA70-6F47-4148-B3E9-3003349C1548}" "Value" "Deny"
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{992AFA70-6F47-4148-B3E9-3003349C1548}" "Value" "Deny"
 
-# Redios: Let apps control radios (like Bluetooth): Allow, Deny
+# Redios: Don't let apps control radios (like Bluetooth): Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{A8804298-2D5F-42E3-9531-9C8C39EB29CE}" "Value" "Deny"
 
-# Other Devices: Let apps share and sync with non-explicitly-paired wireless devices over uPnP: Allow, Deny
+# Other Devices: Don't let apps share and sync with non-explicitly-paired wireless devices over uPnP: Allow, Deny
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled" "Value" "Deny"
 
-# Feedback: Windows should ask for my feedback: Never
+# Feedback: Windows should never ask for my feedback
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" "NumberOfSIUFInPeriod" 0
 
 # Feedback: Telemetry: Send Diagnostic and usage data: Basic: 1, Enhanced: 2, Full: 3
