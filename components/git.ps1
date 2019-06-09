f287d71f49feac0ea97c8ee7132ae9376a857aed
Jay Harris <jay@aranasoft.com>
2019-06-09 13:37:29 -0700

    bug(git): remove ssh agent start
    closes #30

--- components/git.ps1
+++ components/git.ps1
@@ -1,4 +1,3 @@
 if (((Get-Command git -ErrorAction SilentlyContinue) -ne $null) -and ((Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue) -ne $null)) {
   Import-Module Posh-Git
-  Start-SshAgent -Quiet
 }
