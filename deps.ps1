1bd5d4be913543c02590b4974047051d54edc075
Jay Harris <jay@aranasoft.com>
2019-06-09 13:49:50 -0700

    chore: remove unused VSIX code from deps script

--- deps.ps1
+++ deps.ps1
@@ -120,20 +120,3 @@ if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
     curl.exe -L https://bit.ly/janus-bootstrap | bash
 }
 
-
-### Visual Studio Plugins
-if (which Install-VSExtension) {
-    ### Visual Studio 2015
-    # VsVim
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-    # Productivity Power Tools 2015
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
-
-    ### Visual Studio 2013
-    # VsVim
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-    # Productivity Power Tools 2013
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
-    # Web Essentials 2013
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
-}
