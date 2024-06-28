dc99c3472da982fe3413222663f70acc74d178e0
Jay Harris <jay@aranasoft.com>
2024-06-28 11:12:10 -0700

    feat(deps): auto-accept source agreements on WinGet

--- deps.ps1
+++ deps.ps1
@@ -9,29 +9,29 @@ if (!(Verify-Elevated)) {
 }
 
 # system and cli
-winget install Microsoft.WebPICmd                        --silent --accept-package-agreements
-winget install Git.Git                                   --silent --accept-package-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
-winget install OpenJS.NodeJS                             --silent --accept-package-agreements
-winget install Python.Python.3.12                        --silent --accept-package-agreements
-winget install RubyInstallerTeam.Ruby.3.2                --silent --accept-package-agreements
+winget install Microsoft.WebPICmd                        --silent --accept-package-agreements --accept-source-agreements
+winget install Git.Git                                   --silent --accept-package-agreements --accept-source-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
+winget install OpenJS.NodeJS                             --silent --accept-package-agreements --accept-source-agreements
+winget install Python.Python.3.12                        --silent --accept-package-agreements --accept-source-agreements
+winget install RubyInstallerTeam.Ruby.3.2                --silent --accept-package-agreements --accept-source-agreements
 
 # browsers
-winget install Google.Chrome                             --silent --accept-package-agreements
-winget install Mozilla.Firefox                           --silent --accept-package-agreements
-#winget install Opera.Opera                               --silent --accept-package-agreements
+winget install Google.Chrome                             --silent --accept-package-agreements --accept-source-agreements
+winget install Mozilla.Firefox                           --silent --accept-package-agreements --accept-source-agreements
+#winget install Opera.Opera                               --silent --accept-package-agreements --accept-source-agreements
 
 # dev tools and frameworks
-winget install Microsoft.PowerShell                      --silent --accept-package-agreements
-winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements
-winget install Microsoft.SQLServerManagementStudio       --silent --accept-package-agreements
-#winget install Microsoft.VisualStudio.2022.Professional  --silent --accept-package-agreements --override "--wait --quiet --norestart --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
-#winget install JetBrains.dotUltimate                     --silent --accept-package-agreements --override "/SpecificProductNames=ReSharper;dotTrace;dotCover /Silent=True /VsVersion=17.0"
-winget install Vim.Vim                                   --silent --accept-package-agreements
-#winget install WinMerge.WinMerge                         --silent --accept-package-agreements
-winget install Microsoft.AzureCLI                        --silent --accept-package-agreements
-winget install Microsoft.Azure.StorageExplorer            --silent --accept-package-agreements
-#winget install Microsoft.Azure.StorageEmulator            --silent --accept-package-agreements
-#winget install Microsoft.ServiceFabricRuntime            --silent --accept-package-agreements
+winget install Microsoft.PowerShell                      --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.SQLServerManagementStudio       --silent --accept-package-agreements --accept-source-agreements
+#winget install Microsoft.VisualStudio.2022.Professional  --silent --accept-package-agreements --accept-source-agreements --override "--wait --quiet --norestart --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
+#winget install JetBrains.dotUltimate                     --silent --accept-package-agreements --accept-source-agreements --override "/SpecificProductNames=ReSharper;dotTrace;dotCover /Silent=True /VsVersion=17.0"
+winget install Vim.Vim                                   --silent --accept-package-agreements --accept-source-agreements
+#winget install WinMerge.WinMerge                         --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.AzureCLI                        --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.Azure.StorageExplorer            --silent --accept-package-agreements --accept-source-agreements
+#winget install Microsoft.Azure.StorageEmulator            --silent --accept-package-agreements --accept-source-agreements
+#winget install Microsoft.ServiceFabricRuntime            --silent --accept-package-agreements --accept-source-agreements
 
 Refresh-Environment
 
