9216c4584ab3aa186be7cecc92239171afd08a95
Jay Harris <jay@aranasoft.com>
2024-06-28 17:35:32 -0700

    feat(deps): remove SQL 2019

--- deps.ps1
+++ deps.ps1
@@ -22,7 +22,7 @@ winget install Mozilla.Firefox                           --silent --accept-packa
 
 # dev tools and frameworks
 winget install Microsoft.PowerShell                      --silent --accept-package-agreements --accept-source-agreements
-winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements --accept-source-agreements
+#winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.SQLServerManagementStudio       --silent --accept-package-agreements --accept-source-agreements
 #winget install Microsoft.VisualStudio.2022.Professional  --silent --accept-package-agreements --accept-source-agreements --override "--wait --quiet --norestart --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
 #winget install JetBrains.dotUltimate                     --silent --accept-package-agreements --accept-source-agreements --override "/SpecificProductNames=ReSharper;dotTrace;dotCover /Silent=True /VsVersion=17.0"
